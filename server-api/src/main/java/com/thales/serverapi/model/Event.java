df40ccc6be246737c8ea63595974ee49f746d651
ThalesCSoares <th4les.castro@gmail.com>
2022-01-31 12:08:48 -0300

    correção do model

--- server-api/src/main/java/com/thales/serverapi/model/Event.java
+++ server-api/src/main/java/com/thales/serverapi/model/Event.java
@@ -1,13 +1,13 @@
 package com.thales.serverapi.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
@@ -23,8 +23,7 @@ public class Event {
     private Date eventRegister;
 
 
-    @ManyToOne
-    @JoinColumn(name="user_id")
-    private User user;
+    @ManyToMany(mappedBy = "events")
+    private List<User> users;
 
 }
--- server-api/src/main/java/com/thales/serverapi/model/User.java
+++ server-api/src/main/java/com/thales/serverapi/model/User.java
@@ -1,11 +1,14 @@
 package com.thales.serverapi.model;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
@@ -19,6 +22,9 @@ public class User {
     private String name;
     private String document;
     
-    @OneToMany
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name="event_user",
+        joinColumns = @JoinColumn(name="event_id", referencedColumnName="id"),
+        inverseJoinColumns = @JoinColumn(name="user_id",referencedColumnName = "id"))
     private List<Event> events;
 }
