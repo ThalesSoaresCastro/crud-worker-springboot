9792278e8546c3e684c6b4361843ac17c74a6c34
ThalesCSoares <th4les.castro@gmail.com>
2022-01-31 23:32:21 -0300

    feat: event services ok

--- server-api/src/main/java/com/thales/serverapi/repository/EventRepository.java
+++ server-api/src/main/java/com/thales/serverapi/repository/EventRepository.java
@@ -1,9 +1,12 @@
 package com.thales.serverapi.repository;
 
+import java.util.List;
+
+import com.thales.serverapi.model.Client;
 import com.thales.serverapi.model.Event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Long>{
-    
+    List<Event> findByClientId(Client client);
 }
---
+++ server-api/src/main/java/com/thales/serverapi/service/EventService.java
@@ -0,0 +1,17 @@
+package com.thales.serverapi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.thales.serverapi.model.Client;
+import com.thales.serverapi.model.Event;
+
+public interface EventService {
+    Event save(Event event);
+    List<Event> findAll();
+    Optional<Event> findById(Long id);
+    Event update(Event event);
+    void deleteById(Long id);
+
+    List<Event> findEventsByClient(Client client);
+}
